838abba861de25c77fbf0d90dba42bb6c35b8e85
Heloisa Ribeiro <heloisari11@gmail.com>
2022-09-14 17:17:43 -0300

    tentativasprojeto

--- projeto/controladores/inserirProduto.js
+++ projeto/controladores/inserirProduto.js
@@ -8,6 +8,26 @@ let incrementalId = 8;
 const inserirProduto = () => {
   console.log("Id atual: " + incrementalId);
   incrementalId++
+
+  console.log("Digite o NOME do Produto");
+  const NomeProd = readline.question();
+
+  console.log("Digite o PREÇO do Produto");
+  const PrecoProd = readline.question();
+
+  console.log("Digite o QUANTIDADE do Produto");
+  const QuanProd = readline.questionInt();
+
+  console.log("Digite o MARCA do Produto");
+  const MarcaProd = readline.question();
+
+  console.log("Digite o FORNCEDOR do Produto");
+  const FornecedorProd = readline.question();
+
+  console.log("Produto Incluido");
+  console.log(NomeProd,PrecoProd,QuanProd,MarcaProd,FornecedorProd);
+
+
   console.log("Dê enter para voltar");
   readline.question();
   console.clear();
