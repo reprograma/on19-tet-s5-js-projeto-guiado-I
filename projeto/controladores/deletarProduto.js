a24263b596cc2f879fab5d7167fe36a3854c20c3
Shirlene Teles <shiteles@gmail.com>
2022-09-13 21:23:09 -0300

    Add resolução das etapas do projeto

--- projeto/controladores/deletarProduto.js
+++ projeto/controladores/deletarProduto.js
@@ -7,13 +7,30 @@ const { database } = require("../database");
 
 const deletarProduto = () => {
   console.log("Digite um id de um produto para deletar");
-  const idSelecionado = readline.question();
+  const idSelecionado = readline.questionInt();
   console.log("Você selecionou o id: " + idSelecionado);
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
+
+  const posicaoProduto = database.findIndex(produto => produto.id === idSelecionado);
+  console.table(database[posicaoProduto])
+
+  console.log("Deseja mesmo deletar esse produto? [1 - Sim 2 - Nao]")
+  const confirmaDelete = readline.questionInt();
+  
+  if (posicaoProduto > -1){
+    database.splice(posicaoProduto, 1,);
+    }
+
+  if(confirmaDelete === 1) {
+    console.table(database)
+    }else{
+      console.log("Dê enter para voltar ao Menu");
+      readline.question();
+  }
 }
+
+  console.clear();
 
 module.exports = {
   deletarProduto
 }
+
--- projeto/controladores/listarTudo.js
+++ projeto/controladores/listarTudo.js
@@ -7,8 +7,22 @@ const readline = require("readline-sync");
 const { database } = require("../database");
 
 const listarTudo = () => {
-  console.table(database);
-  console.log("Dê enter para voltar");
+
+  const listaItens = database.map(
+    
+    (produto) => {
+      const listado = {
+        id: produto.id,
+        nome: produto.nome,
+        preco: produto.preco,
+        quantidade: produto.quantidade
+      };
+      return listado;
+    }
+  );
+  console.table(listaItens)
+
+  console.log("Dê enter para voltar ao Menu");
   readline.question();
   console.clear();
 }
