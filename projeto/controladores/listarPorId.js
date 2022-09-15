545a0293c1b2def19a7eec9722ec9c0a9d75c84f
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-09-15 00:04:44 -0300

    add controller listarPorID

--- projeto/controladores/listarPorId.js
+++ projeto/controladores/listarPorId.js
@@ -1,16 +1,29 @@
 // Pergunte qual id mostrar detalhes
 // Mostre a tabela com apenas o produto selecionado, com todos os campos
 
-const readline = require("readline-sync");
-const { database } = require("../database");
+const readline = require('readline-sync')
+const { database } = require('../database')
 
 const listarPorId = () => {
-  console.log("Qual produto deseja ver detalhes?");
-  const produtoIdSelecionado = readline.question();
-  console.log("Produto id selecionado: " + produtoIdSelecionado);
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
+  console.log('De qual produto deseja ver detalhes?')
+  const produtoIdSelecionado = readline.questionInt()
+
+  const produtoEncontrado = database.find(
+    produto => produto.id == produtoIdSelecionado
+  )
+  console.table(produtoEncontrado)
+
+  console.log('Deseja visualizar outro produto? [1-SIM, 2-NÃO]')
+  const respostaNovoProduto = readline.questionInt()
+
+  if (respostaNovoProduto === 1) {
+    listarPorId()
+  } else {
+    console.log('Dê um ENTER para voltar ao Menu')
+    readline.question()
+  }
+
+  console.clear()
 }
 
 module.exports = {
