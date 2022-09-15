df0768708fce3eb5e31324de9ef458ccd4c8763f
Karol Barbosa <anna.souza.karoline@gmail.com>
2022-09-15 01:23:14 -0300

    ProjetoSemana5-LógicadeProgramação

--- projeto/controladores/deletarProduto.js
+++ projeto/controladores/deletarProduto.js
@@ -9,7 +9,28 @@ const deletarProduto = () => {
   console.log("Digite um id de um produto para deletar");
   const idSelecionado = readline.question();
   console.log("Você selecionou o id: " + idSelecionado);
-  console.log("Dê enter para voltar");
+  console.log("Tem certeza que você quer excluir o produto selecionado?"); 
+  const nomeIdSelecionado = database.find(item => item.id == +idSelecionado);
+  console.log(nomeIdSelecionado.nome);
+  const confirmar = readline.question(`Atencao: essa acao eh irreversivel
+  Para confirmar a deletacao do produto, digite 1 
+  Caso queira cancelar essa acao aperte enter `);
+   
+  const deletarProduto = () => {
+    const posicaoProduto = database.findIndex(item => item.id == idSelecionado);
+    if (posicaoProduto > -1) {
+      database.splice(posicaoProduto, 1);
+      deletarProduto();
+    }
+  }
+  
+  const resposta = () => (confirmar == 1) ? deletarProduto() : readline.question(`Voce escolheu cancelar a deletacao do produto
+  Agora aperte enter para manter inalterado o produto`);
+  resposta();
+  console.clear();
+  
+ 
+  console.log("Dê enter para voltar ao MENU");
   readline.question();
   console.clear();
 }
