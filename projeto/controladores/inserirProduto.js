67f9f8aa7156b249425f93db1fc4eb6bf72b6dd5
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-09-15 17:35:49 -0300

    add controller inserirProduto

--- projeto/controladores/inserirProduto.js
+++ projeto/controladores/inserirProduto.js
@@ -1,16 +1,43 @@
 // Pergunte ao usuário os dados do novo produto
 // Pergunte um campo por vez, exceto id
 // O id deve ser acrecido pelo sistema automaticamente usando o incrementalId
-const readline = require("readline-sync");
-const { database } = require("../database");
-let incrementalId = 8;
+const readline = require('readline-sync')
+const { database } = require('../database')
+let incrementalId = 8
 
 const inserirProduto = () => {
-  console.log("Id atual: " + incrementalId);
+  console.log('ID atual: ' + incrementalId)
   incrementalId++
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
+
+  console.log('Informe o nome do produto:')
+  const nomeProduto = readline.question()
+
+  console.log('Informe o valor do produto:')
+  const precoProduto = readline.question()
+
+  console.log('Informe a quantidad do produto:')
+  const qtdProduto = readline.question()
+
+  console.log('Informe a marca do produto:')
+  const marcaProduto = readline.question()
+
+  console.log('Informe o nome do fornecedor:')
+  const nomeFornecedor = readline.question()
+
+  const addProduto = {
+    id: incrementalId,
+    nome: nomeProduto,
+    marca: marcaProduto,
+    preco: +precoProduto,
+    quantidade: +qtdProduto,
+    fornecedor: nomeFornecedor
+  }
+
+  database.push(addProduto)
+  console.table(database)
+  console.log('Dê um ENTER para voltar ao Menu')
+  readline.question()
+  console.clear()
 }
 
 module.exports = {
