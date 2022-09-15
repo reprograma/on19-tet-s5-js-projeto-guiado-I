bb9da243f1311c00ff717ac6cf5b00cb3000224f
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-09-15 16:16:06 -0300

    add controller deletarProduto

--- projeto/controladores/deletarProduto.js
+++ projeto/controladores/deletarProduto.js
@@ -2,16 +2,23 @@
 // Pergunte se o usuário realmente deseja deletar o produto X (mostre o nome do produto)
 // Se o usuário confirmar, delete o produto e volte ao menu inicial
 // Se o usuário desistir, volte ao menu inicial
-const readline = require("readline-sync");
-const { database } = require("../database");
+const readline = require('readline-sync')
+const { database } = require('../database')
 
 const deletarProduto = () => {
-  console.log("Digite um id de um produto para deletar");
-  const idSelecionado = readline.question();
-  console.log("Você selecionou o id: " + idSelecionado);
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
+  console.log('Digite o id do produto para deletar')
+  const idSelecionado = readline.question()
+  console.log('Você selecionou o id: ' + idSelecionado)
+  //console.log("Dê enter para voltar");
+  console.log('Tem certeza de que deseja deletar esse produto?[1-SIM, 2-NÃO]')
+  const cond = readline.question()
+  if (cond === '1') {
+    console.log('Produto removido da base de dados')
+  }
+
+  console.log('Dê um ENTER para voltar')
+  readline.question()
+  console.clear()
 }
 
 module.exports = {
