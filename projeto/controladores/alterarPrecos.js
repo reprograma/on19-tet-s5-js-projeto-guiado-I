0641f14eae466021aa7170cb641f01db4e1a7398
Bianca Trindade <biancatrindade.mkt@gmail.com>
2022-09-15 10:37:44 -0300

    teste

--- projeto/controladores/alterarPrecos.js
+++ projeto/controladores/alterarPrecos.js
@@ -8,14 +8,37 @@ const readline = require("readline-sync");
 const { database } = require("../database");
 
 const alterarPrecos = () => {
-  console.log("Qual porcentual deseja aplicar todos os produtos?");
+  console.log("Qual porcentual deseja aplicar todos os produtos?"); // Pergunte qual o porcentual a ser aplicado
   const porcentual = readline.question();
-  console.log("Porcentual: " + porcentual);
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
-}
+
+  console.log("Porcentual: " + porcentual + "%");
+
+  console.log("Você realmente quer aumentar o valor pra tudo isso?"); // Pergunte se o usuário realmente quer aplicar o aumento
+  const daTempoDeDesistir = readline.question();
+
+  if (daTempoDeDesistir.toLowerCase() === "sim") { // Caso prossiga, aplique o porcentual em todos os produtos
+    for (let index = 0; index < database.length; index++) {
+      const calculo = +(database[index].preco* (1 + +porcentual/100)).toFixed(2);
+      database[index].preco = calculo;
+    }
+
+    console.log("\nMore, esse realmente é o governo bolsonaro. To chocada 'O'");
+    console.log("\nOs novos valores são:");
+    console.table(database);
+    console.log("\nDê enter para voltar");
+    readline.question();
+    console.clear();
+
+  } else { // Caso ele desista, volte ao meno inicial
+    console.log("Fez bem more, governo bolsonaro ninguém merece.");
+    console.log("Dê enter para voltar");
+    readline.question();
+    console.clear();
+  }
+};
 
 module.exports = {
   alterarPrecos
 }
+
+alterarPrecos();
--- projeto/controladores/listarPorId.js
+++ projeto/controladores/listarPorId.js
@@ -4,10 +4,14 @@
 const readline = require("readline-sync");
 const { database } = require("../database");
 
-const listarPorId = () => {
-  console.log("Qual produto deseja ver detalhes?");
+const listarPorId = () => {// Pergunte qual id mostrar detalhes
+  console.log("Digite o id do produto que deseja ver os detalhes:");
   const produtoIdSelecionado = readline.question();
-  console.log("Produto id selecionado: " + produtoIdSelecionado);
+  
+  console.log("Você selecionou o id: " + produtoIdSelecionado);
+
+  console.table(database.find(database => database.id === +produtoIdSelecionado)); // Mostre a tabela com apenas o produto selecionado, com todos os campos
+
   console.log("Dê enter para voltar");
   readline.question();
   console.clear();
@@ -16,3 +20,7 @@ const listarPorId = () => {
 module.exports = {
   listarPorId
 }
+
+listarPorId();
+
+  
