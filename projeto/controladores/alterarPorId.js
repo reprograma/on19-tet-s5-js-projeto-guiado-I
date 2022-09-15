fd233af40f67ee58f1c1f26c61cd2e66d276195f
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-09-15 17:22:29 -0300

    add controller alterarPorID

--- projeto/controladores/alterarPorId.js
+++ projeto/controladores/alterarPorId.js
@@ -3,16 +3,42 @@
 // Pergunte cada campo para o usuário, exceto o id
 // altere o produto selecionado
 
-const readline = require("readline-sync");
-const { database } = require("../database");
+const readline = require('readline-sync')
+const { database } = require('../database')
 
 const alterarPorId = () => {
-  console.log("Digite um id de um produto para alterar");
-  const idSelecionado = readline.question();
-  console.log("Você selecionou o id: " + idSelecionado);
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
+  console.log('Digite o id do produto a ser alterado:')
+  const idSelecionado = readline.question()
+
+  const produtoEncontrado = database.findIndex(
+    database => database.id === +idSelecionado
+  )
+
+  const produto = database.find(database => database.id === +idSelecionado)
+  database.splice(produtoEncontrado)
+  console.log('O produto encontrado é: ')
+  console.table(produto)
+
+  const novoNome = readline.question('\nDigite o nome atualizado: ')
+  const novaMarca = readline.question('Digite a marca atualizada: ')
+  const novoPreco = readline.question('Digite preco atualizado: ')
+  const novoFornecedor = readline.question('Digite o fornecedor atualizado: ')
+  const novaQuantidade = readline.question('Digite a quantidade atualizada: ')
+
+  const novoProduto = {
+    nome: novoNome,
+    marca: novaMarca,
+    preco: +novoPreco,
+    fornecedor: novoFornecedor,
+    quantidade: +novaQuantidade
+  }
+
+  console.log('\nO produto alterado é: ')
+  console.table(novoProduto)
+
+  console.log('\nDê um ENTER para voltar ao Menu')
+  readline.question()
+  console.clear()
 }
 
 module.exports = {
