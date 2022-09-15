50fbac6635e988b59c1cb7b8eb2a5ee9b0fc5908
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-09-15 17:11:57 -0300

    add controller alterarPrecos

--- projeto/controladores/alterarPrecos.js
+++ projeto/controladores/alterarPrecos.js
@@ -4,17 +4,34 @@
 // Caso prossiga, aplique o porcentual em todos os produtos
 // Lembre que é necessário converter string para number vindo do terminal
 
-const readline = require("readline-sync");
-const { database } = require("../database");
+const readline = require('readline-sync')
+const { database } = require('../database')
 
 const alterarPrecos = () => {
-  console.log("Qual porcentual deseja aplicar todos os produtos?");
-  const porcentual = readline.question();
-  console.log("Porcentual: " + porcentual);
-  console.log("Dê enter para voltar");
-  readline.question();
-  console.clear();
+  console.log('Qual porcentual deseja aplicar nos produtos?')
+  const porcentual = readline.questionInt()
+  console.log('\nPercentual de aumento: ' + porcentual + '%\n')
+
+  console.log('Tem certeza de que deseja aumentar o preço dos produtos?')
+  const resposta = readline.questionInt('\n[1-SIM, 2-NAO] \n')
+
+  if (resposta === 1) {
+    for (let index = 0; index < database.length; index = index + 1) {
+      const aumento = +(
+        database[index].preco *
+        (1 + +porcentual / 100)
+      ).toFixed(2)
+      database[index].preco = aumento
+    }
+
+    console.table(database)
+    console.log('\n Preços alterados! ')
+  } else {
+    console.log('Dê um ENTER para voltar ao Menu')
+    readline.question()
+  }
 }
+console.clear()
 
 module.exports = {
   alterarPrecos
